bb8650a90ed3702a21be4161a0635ca9c5a12e34
Melanie Sverdlov <nyaadoodle@gmail.com>
2025-03-15 20:39:52 +0200

    NetworkDiscovery Test messages functionality added

--- NetworkDiscovery Testing/Assets/Scripts/MessageBehaviour.cs
+++ NetworkDiscovery Testing/Assets/Scripts/MessageBehaviour.cs
@@ -1,23 +1,51 @@
 using UnityEngine;
 using Mirror;
 using TMPro;
-using UnityEngine.UI;
+using System;
 
 public class MessageBehaviour : NetworkBehaviour
 {
-    [SerializeField] private GameObject messageUI = null;
     [SerializeField] private TMP_Text messageText = null;
     [SerializeField] private TMP_InputField inputField = null;
-    [SerializeField] private Button sendButton = null;
 
-    public override void OnStartAuthority()
+    private static event Action<string> onMessage;
+
+    public override void OnStartLocalPlayer()
     {
-        messageUI.SetActive(true);
+        this.gameObject.SetActive(true);
+        onMessage += handleNewMessage;
     }
 
     [ClientCallback]
-    private void OnDestroy()
+    void OnDestroy()
     {
-        
+        if (!isOwned) { return; }
+        onMessage -= handleNewMessage;
+    }
+
+    [Client]
+    public void Send()
+    {
+        if (string.IsNullOrWhiteSpace(inputField.text)) { return; }
+
+        commandSendMessage(inputField.text);
+        inputField.text = string.Empty;
+    }
+
+    [Command]
+    private void commandSendMessage(string message)
+    {
+        rpcHandleMessage($"[{connectionToClient.connectionId}]: {message}");
+    }
+
+    [ClientRpc]
+    private void rpcHandleMessage(string message)
+    {
+        onMessage?.Invoke($"{Environment.NewLine}{message}");
+    }
+
+    private void handleNewMessage(string message)
+    {
+        messageText.text += message;
     }
 }
