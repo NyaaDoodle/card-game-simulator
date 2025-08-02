a6c1c8753d4b14689ef9ef7bcb47b6fefcc5de0f
NyaaDoodle <nyaadoodle@gmail.com>
2025-08-02 18:03:59 +0300

    Added menu prefabs, button prefabs

---
+++ Card Game Simulator/Assets/Scripts/Simulator/UI/UIManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private RectTransform interactionMenuContainer;
+    [SerializeField] private GameObject deckMenuItemsPrefab;
+    [SerializeField] private GameObject spaceMenuItemsPrefab;
+    [SerializeField] private GameObject CardSelectionMenuItemsPrefab;
+    [SerializeField] private GameObject PlacingCardMenuItemsPrefab;
+    [SerializeField] private GameObject mainMenuItemsPrefab;
+
+    public void displayDeckActionMenu()
+    {
+
+    }
+
+    public void displaySpaceActionMenu()
+    {
+
+    }
+
+    public void displayCardSelectionActionMenu()
+    {
+
+    }
+
+    public void displayPlacingCardMenuItems()
+    {
+
+    }
+
+    public void displayMainMenu()
+    {
+
+    }
+}
